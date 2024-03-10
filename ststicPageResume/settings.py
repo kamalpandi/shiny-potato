13c4d496ecfc92dc9507fe9cf7f22fe0eb0eca38
kamal pandi v <kamalpandi123@gmail.com>
2024-03-10 16:54:10 +0530

    debugging turnd off

--- ststicPageResume/settings.py
+++ ststicPageResume/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c83=p)_=$k&_ny@$=t=uky(wmrhi9sx6r2&0x!(@f7a&*4gaak'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vibi.pythonanywhere.com ']
 
 
 # Application definition

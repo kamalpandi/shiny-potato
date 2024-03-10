bf02163d8044dcef23012cdf24956405269b954e
kamal pandi v <kamalpandi123@gmail.com>
2024-03-10 19:18:47 +0530

    major structural changes 6

--- ststicPageResume/settings.py
+++ ststicPageResume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c83=p)_=$k&_ny@$=t=uky(wmrhi9sx6r2&0x!(@f7a&*4gaak'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
